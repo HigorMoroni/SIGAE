664525fec6bfb78cbf5e33c9f0bbcf868780d056
Higor Moroni <moroni.higor@gmail.com>
2019-09-02 21:33:08 -0300

    Dividindo o repetitivo da index.php em partes/

---
+++ partes/cabmobile.php
@@ -0,0 +1,81 @@
+<header class="header-mobile d-block d-lg-none">
+    <div class="header-mobile__bar">
+        <div class="container-fluid">
+            <div class="header-mobile-inner">
+                <a class="logo" href="index.html">
+                    <img src="images/logoSIGAE.png" alt="SIGAE" />
+                </a>
+                <button class="hamburger hamburger--slider" type="button">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar-mobile">
+        <div class="container-fluid">
+            <ul class="navbar-mobile__list list-unstyled">
+                <li class="has-sub">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-tachometer-alt"></i>Alunos</a>
+                    <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                        <li>
+                            <a href="index.html">Matricular Aluno</a>
+                        </li>
+                        <li>
+                            <a href="index2.html">Lista Piloto</a>
+                        </li>
+                        <li>
+                            <a href="index3.html">Expedir Transferência</a>
+                        </li>
+                        <li>
+                            <a href="index4.html">Histórico Escolar</a>
+                        </li>
+                        <li>
+                            <a href="index5.html">Bolsa Família</a>
+                        </li>
+                        <li>
+                            <a href="index6.html">Consolidados e Deliberações</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-chart-bar"></i>Funcionários</a>
+                    <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                        <li>
+                            <a href="index.html">Cadastrar Funcionário</a>
+                        </li>
+                        <li>
+                            <a href="index2.html">Lista de Funcionários</a>
+                        </li>
+                        <li>
+                            <a href="index3.html">Excluir Funcionários</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="table.html">
+                        <i class="fas fa-table"></i>Alimentação Escolar</a>
+                </li>
+                <li>
+                    <a href="form.html">
+                        <i class="far fa-check-square"></i>Documentação</a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-calendar-alt"></i>Secretaria</a>
+                </li>
+                <li>
+                    <a href="map.html">
+                        <i class="fas fa-map-marker-alt"></i>Quadros</a>
+                </li>
+                <li>
+                    <a href="map.html">
+                        <i class="fas fa-map-marker-alt"></i>Gestão de Entregas</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ partes/menulateral.php
@@ -0,0 +1,72 @@
+<aside class="menu-sidebar d-none d-lg-block">
+    <div class="logo">
+        <a href="#">
+            <img src="images/logoSIGAE.png" alt="Cool Admin" />
+        </a>
+    </div>
+    <div class="menu-sidebar__content js-scrollbar1">
+        <nav class="navbar-sidebar">
+            <ul class="list-unstyled navbar__list">
+                <li class="active has-sub">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-tachometer-alt"></i>Alunos</a>
+                    <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        <li>
+                            <a href="index.html">Matricular Aluno</a>
+                        </li>
+                        <li>
+                            <a href="index2.html">Lista Piloto</a>
+                        </li>
+                        <li>
+                            <a href="index3.html">Expedir Transferência</a>
+                        </li>
+                        <li>
+                            <a href="index4.html">Histórico Escolar</a>
+                        </li>
+                        <li>
+                            <a href="index5.html">Bolsa Família</a>
+                        </li>
+                        <li>
+                            <a href="index6.html">Consolidados e Deliberações</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-chart-bar"></i>Funcionários</a>
+                    <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        <li>
+                            <a href="index.html">Cadastrar Funcionário</a>
+                        </li>
+                        <li>
+                            <a href="index2.html">Lista de Funcionários</a>
+                        </li>
+                        <li>
+                            <a href="index3.html">Excluir Funcionários</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="table.html">
+                        <i class="fas fa-table"></i>Alimentação Escolar</a>
+                </li>
+                <li>
+                    <a href="form.html">
+                        <i class="far fa-check-square"></i>Documentação</a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-calendar-alt"></i>Secretaria</a>
+                </li>
+                <li>
+                    <a href="map.html">
+                        <i class="fas fa-map-marker-alt"></i>Quadros</a>
+                </li>
+                <li>
+                    <a href="map.html">
+                        <i class="fas fa-map-marker-alt"></i>Gestão de Entregas</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
